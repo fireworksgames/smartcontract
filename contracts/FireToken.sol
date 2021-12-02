49cbb6430d6161314121b4ad34f00b3d0b8a01fa
hzmhuhu <leaoucompany@gmail.com>
2021-12-02 16:37:21 +0800

    update maxsupply

--- contracts/FireToken.sol
+++ contracts/FireToken.sol
@@ -5,29 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// The Fire Token
-contract FireToken is ERC20, ERC20Burnable,Ownable {
-      
+contract FireToken is ERC20, ERC20Burnable, Ownable {
+    uint256 MaxSupply = 10 * (10 ** 9) * 10 ** decimals();
 
-    //different blockchain has different block time
-    constructor() ERC20("FireToken", "FIRE") {
+    constructor() ERC20("FireToken", "FIRE")  {}
 
-    _mint(msg.sender,10 * (10 ** 9) * 10 ** decimals());
-
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(MaxSupply >= totalSupply() + amount,"Exceed the MaxSupply!" );
+        _mint(to, amount);
     }
 
-    function mint(address account, uint256 amount) public onlyOwner returns (bool)  {
-        _mint(account,amount);
-        return true;
+    function  maxSupply() public view returns (uint256){
+        return MaxSupply;
     }
 
+    function burn(uint256 amount) public override {
+         _burn(_msgSender(), amount);
+         MaxSupply = MaxSupply - amount;
+    }
     function batchTransfer(address[] calldata to,uint256[] calldata amount)  external {
-        require(to.length == amount.length,"Length of to and amount is not same");
+        require(to.length == amount.length,"Length of to and amount is not same.");
         //require(to)
         for(uint256 i=0;i<to.length;i++){
             transfer(to[i], amount[i]);
         }
     }
-
-
 }
